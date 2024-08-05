65e3b149952489b955a361b297b2b0ebbdfb7142
Pruds52 <prudenciopvp@gmail.com>
2024-08-05 20:31:03 -0300

    Criação do aplicativo e criação do arquivo mapa.jsx

--- geolocalizacao/app/(tabs)/_layout.tsx
+++ geolocalizacao/app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="mapa"
         options={{
-          title: 'Explore',
+          title: 'Mapa',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
